fb1fe3cc854904a1d4ba2f3cf0ffd6f2fa69a34e
Chrispap100 <froutorama.christos@gmail.com>
2025-05-14 13:38:14 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function navigate(section) {
     let content = document.getElementById("content");
     switch(section) {
         case "askiseis":
-            content.innerHTML = "<h2>Ασκήσεις</h2><p>Πόσο κάνει 2 + 3; <br><button onclick='alert("Σωστά!")'>5</button> <button onclick='alert("Δοκίμασε ξανά")'>6</button></p>";
+            content.innerHTML = "<h2>Ασκήσεις</h2><p>Πόσο κάνει 2 + 3; <br><button onclick=\"alert('Σωστά!')\">5</button> <button onclick=\"alert('Δοκίμασε ξανά')\">6</button></p>";
             break;
         case "paromythia":
-            content.innerHTML = "<h2>Ερωτήσεις Παραμυθιών</h2><p>Ποια είναι η αδελφή της Έλσας στο Frozen; <br><button onclick='alert("Σωστά! Η Άννα!")'>Άννα</button> <button onclick='alert("Δοκίμασε ξανά")'>Μαρία</button></p>";
+            content.innerHTML = "<h2>Ερωτήσεις Παραμυθιών</h2><p>Ποια είναι η αδελφή της Έλσας στο Frozen; <br><button onclick=\"alert('Σωστά! Η Άννα!')\">Άννα</button> <button onclick=\"alert('Δοκίμασε ξανά')\">Μαρία</button></p>";
             break;
         case "tiemathematoday":
-            content.innerHTML = "<h2>Τι έμαθες σήμερα;</h2><textarea rows='4' cols='40' placeholder='Γράψε εδώ...'></textarea><br><button onclick='alert("Μπράβο!")'>Αποθήκευση</button>";
+            content.innerHTML = "<h2>Τι έμαθες σήμερα;</h2><textarea rows='4' cols='40' placeholder='Γράψε εδώ...'></textarea><br><button onclick=\"alert('Μπράβο!')\">Αποθήκευση</button>";
             break;
         case "agapimena":
             content.innerHTML = "<h2>Αγαπημένα</h2><p>Εδώ θα εμφανίζονται όσα αγαπάς!</p>";
